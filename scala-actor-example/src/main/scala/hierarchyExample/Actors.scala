349b2fe6a5f96a2ee53ccb3ddbcbb95902576efd
Michael Meister <micmeist@htwg-konstanz.de>
2016-04-18 15:21:27 +0200

    Refactor

--- scala-actor-example/src/main/scala/hierarchyExample/Actors.scala
+++ scala-actor-example/src/main/scala/hierarchyExample/Actors.scala
@@ -8,15 +8,12 @@ import akka.actor.{Props, Actor}
 class ParentActor extends Actor {
 
   var n: Int = 0
-  val almightyChild = context.actorOf(Props[AlmightyChildActor], "almightyChild")
 
   override def receive = {
     case hm: HelloMessage =>
       val child = context.actorOf(Props[HelloChildActor], "child" + n)
       n += 1
       child.tell(HelloMessage(hm.message), self)
-    case DangerousMessage =>
-      almightyChild ! DangerousMessage
   }
 
 }
@@ -26,17 +23,6 @@ class HelloChildActor extends Actor {
   override def receive = {
     case hm: HelloMessage => println(s"You said: ${hm.message}")
       context.stop(self)
-    case _ => println("What?")
-      context.stop(self)
-  }
-
-}
-
-class AlmightyChildActor extends Actor {
-
-  override def receive = {
-    case DangerousMessage => println("I do something dangerous")
-    case _ => println("What?")
   }
 
 }
--- scala-actor-example/src/main/scala/hierarchyExample/HelloMessage.scala
+++ scala-actor-example/src/main/scala/hierarchyExample/HelloMessage.scala
@@ -4,5 +4,3 @@ package hierarchyExample
   * Created by Michael Meister on 16.04.2016.
   */
 case class HelloMessage(message: String)
-
-case object DangerousMessage
